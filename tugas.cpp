e28431570439e1f593ceffe11e1e39f54e097e3a
dindaazra <azra070506@gmail.com>
2024-10-02 11:09:13 +0700

    Update tugas.cpp
    ini update

--- tugas.cpp
+++ tugas.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-#include <cstdlib>  
-#include <ctime>    
-
-using namespace std;
-
-string getPilihan() {
-    string pilihan;
-    cout << "Masukkan pilihan Anda (gunting, batu, kertas): ";
-    cin >> pilihan;
-    return pilihan;
+tring komputer() {
+    int random = rand() % 3;
+    if (random == 0) return "gunting";
+    else if (random == 1) return "batu";
+    else return "kertas";
 }
 
+string pemenang(string pemain, string komputer) {
+    if (pemain == komputer) return "Seri!";
+    else if ((pemain == "gunting" && komputer == "kertas") || 
+             (pemain == "batu" && komputer == "gunting") || 
+             (pemain == "kertas" && komputer == "batu")) {
+        return "Anda menang!";
+    } else {
