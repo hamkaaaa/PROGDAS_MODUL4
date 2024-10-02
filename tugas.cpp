e47f19414d8b655039d65c64fe3c1a43dde6f1f4
hamkaaaa <hamkamuhammad5@gmail.com>
2024-10-02 11:11:25 +0700

    Update tugas.cpp

--- tugas.cpp
+++ tugas.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-tring komputer() {
+#include <cstdlib>  
+#include <ctime>    
+
+using namespace std;
+
+string getPilihan() {
+    string pilihan;
+    cout << "Masukkan pilihan Anda (gunting, batu, kertas): ";
+    cin >> pilihan;
+    return pilihan;
+}
+
+string komputer() {
     int random = rand() % 3;
     if (random == 0) return "gunting";
     else if (random == 1) return "batu";
