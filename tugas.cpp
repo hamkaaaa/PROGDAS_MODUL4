42398671b331f01d06e6dde41b7db0fcfeb810d3
aureliap2122 <aureliacahyani3@gmail.com>
2024-10-02 10:54:25 +0700

    Update tugas.cpp
    ini updatenya

--- tugas.cpp
+++ tugas.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cstdlib>  
+#include <ctime>    
+
+using namespace std;
+
+string getPilihan() {
+    string pilihan;
+    cout << "Masukkan pilihan Anda (gunting, batu, kertas): ";
+    cin >> pilihan;
+    return pilihan;
+}
+
+int main() {
+    // Contoh penggunaan fungsi getPilihan
+    string pilihanUser = getPilihan();
+    cout << "Anda memilih: " << pilihanUser << endl;
+
+    return 0;
+}
 
